99e7f3325c23e7847fb66edb13d8e61476321078
Unknown <dom.t@live.co.uk>
2018-02-17 12:00:18 +0000

    Small changes

--- Assets/Scripts/Initializer.cs
+++ Assets/Scripts/Initializer.cs
@@ -7,6 +7,7 @@ public class Initializer : MonoBehaviour {
 
     public Game game;
  
+    //Modified by Ryan
     /// <summary>
     /// 
     /// start the game
@@ -17,19 +18,18 @@ public class Initializer : MonoBehaviour {
         switch (PlayerPrefs.GetInt("_gamemode", 0))
         {
             case 0:
-                game.Initialize(false);
+                game.Initialize(false); // start new game with no neutral player
                 break;
             case 1:
-                game.Initialize(true);
+                game.Initialize(true); // start new game with neutal player
                 break;
             case 2:
-                game.Initialize(SavedGame.Load("test1"));
+                game.Initialize(SavedGame.Load("test1")); // load saved game
                 break;
             case 3:
-                game.Initialize(SavedGame.Load("_tmp"));
+                game.Initialize(SavedGame.Load("_tmp")); // load back from the mini-game
                 game.GiveReward();
                 break;
         }
-        //game.Initialize();
     }
 }
