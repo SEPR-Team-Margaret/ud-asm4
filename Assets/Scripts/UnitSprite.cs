d567bbd49230086a51c968d511a1318e930ce6f6
David Clarke <dclarkeu@gmail.com>
2018-03-21 18:20:30 +0000

    Added units stuff

---
+++ Assets/Scripts/UnitSprite.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSprite {
+    private GameObject unitGO;
+
+    private GameObject headGO;
+    [SerializeField] private int currentHead;
+
+    private GameObject bodyGO;
+    [SerializeField] private int currentBody;
+
+    private GameObject hatGO;
+    [SerializeField] private int currentHat;
+
+    public UnitSprite(GameObject unit, int head = 0, int body = 0, int hat = 0) {
+        unitGO = unit;
+
+        headGO = unitGO.transform.Find("Head").gameObject;
+        bodyGO = unitGO.transform.Find("Body").gameObject;
+        hatGO = unitGO.transform.Find("Hat").gameObject;
+
+        SetHead(head);
+        SetBody(body);
+        SetHat(hat);
+    }
+
+    public void SetHead(int headID) {
+        MeshRenderer renderer = this.headGO.GetComponent<MeshRenderer>();
+        string resourceLC = "UnitSprites/Head/head" + headID.ToString() + ".mat";
+        renderer.material = (Material)Resources.Load(resourceLC);
+    }
+
+    public void SetBody(int bodyID) {
+        MeshRenderer renderer = this.bodyGO.GetComponent<MeshRenderer>();
+        string resourceLC = "UnitSprites/Body/body" + bodyID.ToString() + ".mat";
+        renderer.material = (Material)Resources.Load(resourceLC);
+    }
+
+    public void SetHat(int hatID) {
+        MeshRenderer renderer = this.hatGO.GetComponent<MeshRenderer>();
+        string resourceLC = "UnitSprites/Hat/hat" + hatID.ToString() + ".mat";
+        renderer.material = (Material)Resources.Load(resourceLC);
+    }
+}
+
